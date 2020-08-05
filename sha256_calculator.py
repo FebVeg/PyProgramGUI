d5e4442ba8edace8352fc1eaf8a861f7f03057ff
FebVeg <feb19958@gmail.com>
2020-08-05 14:49:39 +0200

    Update sha256_calculator.py

--- sha256_calculator.py
+++ sha256_calculator.py
@@ -1,30 +1,69 @@
-import hashlib
-import PySimpleGUI as sg
+# -*- coding: utf-8 -*-
 
-layout = [
-    [sg.Frame('SHA256',[
-        [sg.Text('File', size=(5, 1)), sg.Input(key="_FILE_"), sg.FileBrowse(button_text="Open", size=(5, 1))],
-        [sg.Button('Calculate')]
-    ])]
-]
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
 
-window = sg.Window('SHA256 Calculator - FebVeg', layout)
 
-while True:
-    event, values = window.read()
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-    if event == sg.WINDOW_CLOSED:
-        break
-    
-    elif event == 'Calculate':
-        filename = values['_FILE_']
-        sha256_hash = hashlib.sha256()
-        # Apro il file in modalità RB
-        with open(filename, "rb") as _file:
-            # Leggo i blocchi
-            for blocco_byte in iter(lambda: _file.read(4096), b""):
-                sha256_hash.update(blocco_byte)
-            # Printo il risultato
-            sg.Print(sha256_hash.hexdigest())
 
-window.close()
+class Ui_MainWindow(object):
+    def open_file(self):
+        from PyQt5.QtWidgets import (QApplication, QWidget, QTextEdit, QPushButton,
+                                    QFileDialog, QMainWindow, QVBoxLayout)
+        import hashlib
+        
+        fd = QFileDialog()
+        nome = fd.getOpenFileName(parent=None, caption="Open File", directory="")
+        nome = nome[0]
+        self.documento.setText(nome)
+        with open(nome, "rb") as f:
+            bytes = f.read() # read entire file as bytes
+            readable_hash = hashlib.sha256(bytes).hexdigest()
+            self.output.setText(readable_hash)
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(511, 103)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.documento = QtWidgets.QLineEdit(self.centralwidget)
+        self.documento.setGeometry(QtCore.QRect(10, 10, 391, 21))
+        self.documento.setObjectName("documento")
+        self.browse = QtWidgets.QPushButton(self.centralwidget)
+        self.browse.setGeometry(QtCore.QRect(430, 10, 71, 23))
+        self.browse.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.browse.setObjectName("browse")
+        self.browse.clicked.connect(self.open_file)
+        self.output = QtWidgets.QLineEdit(self.centralwidget)
+        self.output.setGeometry(QtCore.QRect(10, 40, 491, 21))
+        self.output.setObjectName("output")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 511, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "SHA256 Calculator"))
+        self.browse.setText(_translate("MainWindow", "File"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
